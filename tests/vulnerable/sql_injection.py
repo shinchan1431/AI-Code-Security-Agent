b8d26195036fe489225f13e65ecb1f948e21ec98
shinsu <ajithkumar472004@gmail.com>
2026-08-19 19:18:34 +0530

    Update sql_injection.py

--- tests/vulnerable/sql_injection.py
+++ tests/vulnerable/sql_injection.py
@@ -1,1 +1,13 @@
 
+import sqlite3
+
+
+def find_user(username):
+    connection = sqlite3.connect("users.db")
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+
+    cursor.execute(query)
+
+    return cursor.fetchall()
